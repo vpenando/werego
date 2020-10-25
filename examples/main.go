67827e99f4c606c438f2bc73aec1be13ce7897cf
vpenando <5292373+vpenando@users.noreply.github.com>
2020-10-25 16:19:33 +0100

    Create main.go

---
+++ examples/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/vpenando/werego"
+)
+
+var (
+	bot *werego.WereBot
+)
+
+func init() {
+	var t string
+	flag.StringVar(&t, "t", "", "Bot token")
+	flag.Parse()
+	var err error
+	bot, err = werego.NewWereBot(t)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	err = bot.Bot.Open()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func main() {
+	defer bot.Bot.Close()
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+}
